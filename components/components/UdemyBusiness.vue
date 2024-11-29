5b46a01e1f75b50ecb6bbfe80a63419c79ad1a02
gülcan <dgulcan88@gmail.com>
2024-11-29 03:23:47 +0300

    8.commit.UdemyBusiness ekledim.

---
+++ components/components/UdemyBusiness.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="component-margin cpa-4">
+        <div class="tou-wrapper">
+            <div class="d-flex">
+                <div class="tou-content">
+                    <img class="cmb-16" src="@/assets/images/logo-ub.svg" width="192" height="32">
+                    <p class="ud-text-md cmb-16 cfs-19">Get unlimited access to 19,000+ of Udemy’s top courses for your
+                        team. Learn and improve skills across business, tech, design, and more.</p>
+                    <div class="d-flex">
+                        <a href="#!" class="black-btn">Get Udemy Business</a>
+                    </div>
+                </div>
+                <img class="tou-img2" width="400" height="400" src="@/assets/images/ub-1x-v3.jpg">
+            </div>
+        </div>
+    </div>
+</template>
