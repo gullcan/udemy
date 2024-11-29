ff8ea0376744eb76c8c862a942f4b19c32fb7258
gülcan <dgulcan88@gmail.com>
2024-11-29 03:15:56 +0300

    my third commit.Billboard ekledim.

---
+++ components/components/Billboard.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <div class="billboard-wrapper">
+            <img width="1340" height="400" alt="" src="@/assets/images/billboard-bg.jpg">
+            <div class="billboard-content-box">
+                <h1 class="heading-serif-xxl">It's the last day for this sale</h1>
+                <p class="p-billboard">Log in to see how much you can save on highly rated courses.</p>
+            </div>
+        </div>
+    </div>
+</template>
