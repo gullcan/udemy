dfa83b19d50f149a796a1b49fc015898e9a71298
gülcan <dgulcan88@gmail.com>
2024-11-29 03:24:45 +0300

    9.commit.UserStories ekledim.

---
+++ components/components/UserStories.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="component-margin">
+        <div class="tou-wrapper">
+            <div class="d-flex">
+                <img class="tou-img" width="400" height="400" src="@/assets/images/transform-1x-v3.jpg">
+                <div class="tou-content">
+                    <h3 class="heading-serif-xxl cmb-16">Transform your life through education</h3>
+                    <p class="ud-text-md cmb-16 cfs-19">Learners around the world are launching new careers, advancing
+                        in their fields, and enriching their lives.</p>
+                    <div class="d-flex">
+                        <a href="#!" class="black-btn">Find out how</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
