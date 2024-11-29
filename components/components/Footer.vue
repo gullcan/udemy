5d43afe6bbc6abdbe89bb5380f59d396e6f8ec2b
gülcan <dgulcan88@gmail.com>
2024-11-29 03:13:29 +0300

    my second commmit.Footer ekledim.

---
+++ components/components/Footer.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <footer class="footer-wrapper">
+            <div class="links-and-languages">
+                <div class="links-column">
+                    <a href="#!">Udemy Business</a>
+                    <a href="#!">Teach on Udemy</a>
+                    <a href="#!">Get the app</a>
+                    <a href="#!">About us</a>
+                    <a href="#!">Contact us</a>
+                </div>
+                <div class="links-column">
+                    <a href="#!">Careers</a>
+                    <a href="#!">Blog</a>
+                    <a href="#!">Help and Support</a>
+                    <a href="#!">Affiliate</a>
+                    <a href="#!">Investors</a>
+                </div>
+                <div class="links-column">
+                    <a href="#!">Terms</a>
+                    <a href="#!">Privacy policy</a>
+                    <a href="#!">Cookie settings</a>
+                    <a href="#!">Sitemap</a>
+                    <a href="#!">Accessibility statement</a>
+                </div>
+                <button class="white-btn2 align-self-start ms-auto">
+                    <v-icon class="me-2">mdi-web</v-icon>
+                    English
+                </button>
+            </div>
+            <div class="logo-and-copyright">
+                <img src="@/assets/images/logo-udemy-inverted.svg" width="91.07142857142857" height="34">
+                <p>© 2022 Udemy, Inc.</p>
+            </div>
+        </footer>
+    </div>
+</template>
