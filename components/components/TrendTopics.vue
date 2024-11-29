bb7c40df435004005ebe6fe3872b91864cb4a5ab
gülcan <dgulcan88@gmail.com>
2024-11-29 03:22:45 +0300

    7.commit.TrendTopics ekledim.

---
+++ components/components/TrendTopics.vue
@@ -0,0 +1,78 @@
+<template>
+    <div class="component-margin section-bg">
+        <div class="ud-container section-padding">
+            <h2 class="ud-heading-xl cmb-24">Featured topics by category</h2>
+            <div class="d-flex justify-space-between flex-wrap">
+                <div class="topic-category">
+                    <h3 class="ud-heading-md cmb-24">Development</h3>
+                    <div>
+                        <div class="d-flex flex-column">
+                            <a href="#!" class="topic-link">Python</a>
+                            <span class="topic-count cmt-08">36,354,994 students</span>
+                        </div>
+                        <div class="d-flex flex-column cmt-24">
+                            <a href="#!" class="topic-link">Web Development</a>
+                            <span class="topic-count cmt-08">11,415,615 students</span>
+                        </div>
+                        <div class="d-flex flex-column cmt-24">
+                            <a href="#!" class="topic-link">Machine Learning</a>
+                            <span class="topic-count cmt-08">7,070,015 students</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="topic-category">
+                    <h3 class="ud-heading-md cmb-24">Business</h3>
+                    <div>
+                        <div class="d-flex flex-column">
+                            <a href="#!" class="topic-link">Financial Analysis</a>
+                            <span class="topic-count cmt-08">1,195,282 students</span>
+                        </div>
+                        <div class="d-flex flex-column cmt-24">
+                            <a href="#!" class="topic-link">SQL</a>
+                            <span class="topic-count cmt-08">5,977,561 students</span>
+                        </div>
+                        <div class="d-flex flex-column cmt-24">
+                            <a href="#!" class="topic-link">PMP</a>
+                            <span class="topic-count cmt-08">1,733,398 students</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="topic-category">
+                    <h3 class="ud-heading-md cmb-24">IT and Software</h3>
+                    <div>
+                        <div class="d-flex flex-column">
+                            <a href="#!" class="topic-link">AWS Certification</a>
+                            <span class="topic-count cmt-08">6,078,244 students</span>
+                        </div>
+                        <div class="d-flex flex-column cmt-24">
+                            <a href="#!" class="topic-link">Ethical Hacking</a>
+                            <span class="topic-count cmt-08">10,931,066 students</span>
+                        </div>
+                        <div class="d-flex flex-column cmt-24">
+                            <a href="#!" class="topic-link">Cyber Security</a>
+                            <span class="topic-count cmt-08">3,998,037 students</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="topic-category">
+                    <h3 class="ud-heading-md cmb-24">Design</h3>
+                    <div>
+                        <div class="d-flex flex-column">
+                            <a href="#!" class="topic-link">Photoshop</a>
+                            <span class="topic-count cmt-08">10,909,736 students</span>
+                        </div>
+                        <div class="d-flex flex-column cmt-24">
+                            <a href="#!" class="topic-link">Graphic Design</a>
+                            <span class="topic-count cmt-08">3,381,052 students</span>
+                        </div>
+                        <div class="d-flex flex-column cmt-24">
+                            <a href="#!" class="topic-link">Drawing</a>
+                            <span class="topic-count cmt-08">2,410,849 students</span>
+                        </div>
+                    </div>
+                </div>
+                <button class="white-btn">Explore more topics</button>
+            </div>
+        </div>
+    </div>
+</template>
