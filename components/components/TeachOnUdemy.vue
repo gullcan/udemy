0e8466d7007887e450337730003476024fb77eae
gülcan <dgulcan88@gmail.com>
2024-11-29 03:21:40 +0300

    6.commit.TeachOnUdemy ekledim.

---
+++ components/components/TeachOnUdemy.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="component-margin">
+        <div class="tou-wrapper">
+            <div class="d-flex">
+                <img class="tou-img" width="400" height="400" src="@/assets/images/teachonudemy.jpg">
+                <div class="tou-content">
+                    <h3 class="heading-serif-xxl cmb-16">Become an instructor</h3>
+                    <p class="ud-text-md cmb-16 cfs-19">Instructors from around the world teach millions of students on
+                        Udemy. We provide the tools and skills to teach what you love.</p>
+                    <div class="d-flex">
+                        <a href="#!" class="black-btn">Start teaching today</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
